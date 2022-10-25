0b21ecc26dea5fe25decaf24ab2e02b044900a87
Mairon Donis <myron2089@gmail.com>
2022-10-25 02:05:20 -0400

    commit update

--- js/chunk-26d7968c.5d82aa5c.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-26d7968c"],{"20f6":function(n,c,o){},"4b85":function(n,c,o){}}]);
-//# sourceMappingURL=chunk-26d7968c.5d82aa5c.js.map
